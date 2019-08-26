a3d10a100815dba5434c7fba4bfe847c6b1a6579
jinxintang <jinxintang@iyuedian.com>
2019-08-26 20:07:07 +0800

    添加文章增删该查看

--- src/setting.ts
+++ src/setting.ts
@@ -2,7 +2,7 @@ export default{
     // 虚拟数据地址
     // baseURL: 'http://39.107.42.37:8080',
     // baseURL: 'https://www.easy-mock.com/mock/5c175b6c899b39035c50fd94/example',
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://39.107.42.37:1234/api',
     // 项目地址
     project: 'https://github.com/2384830985/iViewCounter/tree/master/my-project',
     debug: true,
